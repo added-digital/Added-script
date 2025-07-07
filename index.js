65451fbc7bc8b87b974479049b7a5ba9d0723105
Oliver-lundkvist <oliver@added.digital>
2025-07-07 14:47:18 +0200

    uppdate

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("[data-word-reveal='true']").forEach((text) => {
-
-    const split = SplitText.create(text.children, {
-      type: "words, chars",
-      mask: "words",
-      wordsClass: "word",
-      charsClass: "char",
-    });
-
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: text,
-        start: "top bottom",
-        end: "top 80%",
-        toggleActions: "none play none reset",
+document.fonts.ready.then(() => {
+  document.querySelectorAll("[data-line-reveal='true']").forEach((text) => {
+    SplitText.create(text, {
+      type: "lines",
+      autoSplit: true,
+      mask: "lines",
+      linesClass: "line",
+      onSplit(self) {
+        return gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: text,
+              start: "top bottom",
+              end: "top 80%",
+              toggleActions: "none play none reset",
+            },
+          })
+          .from(self.lines, {
+            yPercent: 110,
+            delay: 0.2,
+            duration: 0.8,
+            stagger: { amount: 0.5 },
+          });
       },
-    });
-    tl.from(split.words, {
-      yPercent: 110,
-      delay: 0.2,
-      duration: 0.8,
-      stagger: { amount: 0.5 },
     });
 
     gsap.set(text, { visibility: "visible" });
