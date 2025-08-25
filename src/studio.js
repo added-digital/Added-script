01d604b5c9661ada98a7627909feb8dfee5ed7bb
Oliver-lundkvist <oliver@added.digital>
2025-08-25 14:27:19 +0200

    test

--- src/studio.js
+++ src/studio.js
@@ -2,3 +2,5 @@ require("./components/about-hero").about_hero();
 require("./components/added_numbers").added_numbers();
 require("./components/service").service_scroll();
 require("./components/service").service_headline();
+
+console.log("hej");
