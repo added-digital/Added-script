8bb9b18981d9bf4748cfd9886d6d3d0a83500f2b
Oliver-lundkvist <oliver@added.digital>
2025-08-25 18:03:50 +0200

    test

--- src/components/nav.js
+++ src/components/nav.js
@@ -70,8 +70,8 @@ exports.nav = function () {
         // Close menu
         // Fade out each link first
         gsap.to(navLinksHamburger, {
-          opacity: 0,
-          y: -20,
+          opacity: 1,
+          y: 0,
           duration: 0.3,
           stagger: 0.05,
           ease: "power2.out",
