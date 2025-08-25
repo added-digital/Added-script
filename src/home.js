758c81dd51e9e38e3eedf81600c2eb513537def0
Oliver-lundkvist <oliver@added.digital>
2025-08-25 15:49:47 +0200

    update

--- src/home.js
+++ src/home.js
@@ -5,4 +5,4 @@ require("./components/service").service_headline();
 require("./components/d-animation").d_animation();
 require("./reusables/blogCard").blogCard();
 require("./components/project_card").project_card();
-
+require("./components/added_numbers").added_numbers();
