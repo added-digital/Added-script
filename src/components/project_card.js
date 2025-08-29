cc94564e78e492af141aab8e37892c2292333477
Oliver-lundkvist <oliver@added.digital>
2025-08-29 14:11:59 +0200

    test

--- src/components/project_card.js
+++ src/components/project_card.js
@@ -11,7 +11,7 @@ exports.project_card = function () {
         ease: "power2.out",
         scrollTrigger: {
           trigger: card,
-          start: "top 100%",
+          start: "top 20%",
           end: "bottom 20%",
           toggleActions: "play none none none",
         },
