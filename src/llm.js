690cd56ec47cddfbc9f96d42d7b5aca3504a1aaf
Oliver-lundkvist <oliver@added.digital>
2025-10-24 09:42:46 +0200

    update

--- src/llm.js
+++ src/llm.js
@@ -1,7 +1,5 @@
 require("./components/added_numbers").added_numbers();
 
-document.fonts.ready.then(() => {
-  gsap.set(".section_logo-slider .section_campaign-hero", {
-    visibility: "visible",
-  });
+gsap.set(".section_logo-slider .section_campaign-hero", {
+  visibility: "visible",
 });
