fc669eafb88cc04cbb386cb69e6ad8b14f3e127e
Oliver-lundkvist <oliver@added.digital>
2025-11-20 16:12:19 +0100

    update

--- src/index.js
+++ src/index.js
@@ -202,6 +202,7 @@ const pageTransitionConfig = {
   "/blog": "light-mode",
   "/sv/blog": "light-mode",
   "/contact": "",
+  "/work/neumeister": "",
 };
 
 // Folder-based class configuration for efficiency
